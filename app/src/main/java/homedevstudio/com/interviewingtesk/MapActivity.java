c58d375d03e854d08c634fe5e8a3d19dbf5eec20
ilya <ilya@flashdev.od.ua>
2017-09-05 22:40:01 +0300

    added random location

--- app/src/main/java/homedevstudio/com/interviewingtesk/MapActivity.java
+++
@@ -1,8 +0,0 @@
-package homedevstudio.com.interviewingtesk;
-
-/**
- * Created by xdewnik on 05.09.2017.
- */
-
-public class MapActivity {
-}
--- app/src/main/java/homedevstudio/com/interviewingtesk/MapsActivity.java
+++ app/src/main/java/homedevstudio/com/interviewingtesk/MapsActivity.java
@@ -1,7 +1,13 @@
 package homedevstudio.com.interviewingtesk;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,37 +16,85 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @BindView(R.id.randomLocationButton)FloatingActionButton randomLocationButton;
     private GoogleMap mMap;
+    private double randomLatValue;
+    private double randomLngValue;
+    private double latOdessa;
+    private double lngOdessa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        ButterKnife.bind(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        try {
+            goToGeo("Одесса");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void goToLocationZoom(double lat, double lng, int zoom){
+
+        LatLng odessa = new LatLng(lat,lng);
+        mMap.addMarker(new MarkerOptions().position(odessa).title("Marker in Odessa"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(odessa,zoom));
+    }
+
+    private void goToGeo(String placeName) throws IOException{
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName(placeName,1);
+        Address address = list.get(0);
+
+        latOdessa = address.getLatitude();
+        lngOdessa = address.getLongitude();
+
+        goToLocationZoom(latOdessa, lngOdessa, 13);
+
+
+    }
+
+    private void generateRandomCoords(double lat, double lng){
+        double latKmValue = 0.00898 * 13;
+        double lngKmValue = 0.01440 * 13;
+
+        double latMin = lat - latKmValue;
+        double latMax = lat + latKmValue;
+        double lngMin = lng - lngKmValue;
+        double lngMax = lng + lngKmValue;
+
+        Random r = new Random();
+         randomLatValue = latMin + (latMax - latMin) * r.nextDouble();
+         randomLngValue = lngMin + (lngMax - lngMin) * r.nextDouble();
+
+    }
+
+    @OnClick(R.id.randomLocationButton)
+    public void onRandomButtonClick(){
+        generateRandomCoords(latOdessa,lngOdessa);
+        goToLocationZoom(randomLatValue, randomLngValue, 13);
+
     }
 }
